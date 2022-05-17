b0dffa00a52a4c0b034c4d343270b529de742da8
Denis Feliciano <denisf.salles@gmail.com>
2022-05-17 20:00:41 -0300

    remove example

--- examples-in-class/web-api/src/entities/hero.js
+++
@@ -1,9 +0,0 @@
-import { randomUUID } from 'node:crypto'
-export default class Hero {
-  constructor({ name, age, power }) {
-    this.id = randomUUID()
-    this.name = name
-    this.age = age
-    this.power = power
-  }
-}
--- examples-in-class/web-api/src/util/util.js
+++
@@ -1,5 +0,0 @@
-const DEFAULT_HEADER = { 'content-type': 'application/json'}
-
-export {
-  DEFAULT_HEADER
-}
